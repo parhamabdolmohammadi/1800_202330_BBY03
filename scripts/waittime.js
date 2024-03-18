f2ac4f9a2574fbba9786e97083cf76aef2dca151
JJasonHong <jasonr8hong@hotmail.ca>
2024-03-18 16:31:26 -0700

    Added the waitTime.js to read the wait time from firebase

---
+++ scripts/waittime.js
@@ -0,0 +1,34 @@
+function readTimeNW(id) {
+    db.collection("quotes").doc(id)                                                      //name of the collection and documents should matach excatly with what I have in Firestore
+      .onSnapshot(idDoc => {                                                               //arrow notation
+           console.log("current time data: " + idDoc.data());                          //.data() returns data object
+      })
+}
+readQuote("FdbvfQwFmYyugTGpbgQc");        //calling the function
+
+function readTimeVGH(id) {
+    db.collection("quotes").doc(id)                                                      //name of the collection and documents should matach excatly with what I have in Firestore
+      .onSnapshot(idDoc => {                                                               //arrow notation
+           console.log("current time data: " + idDoc.data());                          //.data() returns data object
+      })
+}
+readQuote("G2yHk50SycGPcyd2ZR3M");        //calling the function
+
+function readTimeBUR(id) {
+    db.collection("quotes").doc(id)                                                      //name of the collection and documents should matach excatly with what I have in Firestore
+      .onSnapshot(idDoc => {                                                               //arrow notation
+           console.log("current time data: " + idDoc.data());                          //.data() returns data object
+      })
+}
+readQuote("LcOuad5poxGgwFB8KKbn");        //calling the function
+
+//Getting all documents from one collection in Firestore
+function getAllItems() {
+    db.collection("hospitals")
+      .get()
+      .then((allitems) => {
+        const sales_item = allitems.docs.map((doc) => doc.data());
+        console.log(sales_item);
+      });
+  }
+  getAllItems();
